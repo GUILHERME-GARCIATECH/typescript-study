3af5368884bd2dba7ad3501218336be02fd3b11f
Guilherme Garcia <guilherme.garcia@assessoriatech.com>
2026-09-11 21:10:31 -0400

    Aula 04

---
+++ src/04.ts
@@ -0,0 +1,6 @@
+let nome = "João";
+let idade = 20;
+let curso = "Engenharia";
+console.log(nome);
+console.log(idade);
+console.log(curso);
--- transpilação/02.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const teste = "Hello, World!";
-const outroteste = "This is a test.";
-console.log(teste);
-console.log(outroteste);
